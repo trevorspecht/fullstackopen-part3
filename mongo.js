ae77f0ba65fb01b2fe14ded57bcb675f64d3c5ea
trevorspecht <trevor.specht@gmail.com>
2021-11-24 17:49:06 -0500

    3.12: command-line database

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://fsouser:${password}@cluster0.fggg3.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 3) {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person)
+        })
+        mongoose.connection.close()
+    })
+} else {
+
+    const person = new Person({
+        name: name,
+        number: number
+      })
+      
+      person.save().then(result => {
+        console.log('person saved!: ', result.name, result.number)
+        mongoose.connection.close()
+      })
+}
+
+
