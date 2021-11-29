125c4a2d72303cf1991855cd0eac7daeb702c82a
trevorspecht <trevor.specht@gmail.com>
2021-11-28 22:36:44 -0500

    3.20: Phonebook database, step 8

--- models/person.js
+++ models/person.js
@@ -17,11 +17,13 @@ const personSchema = new mongoose.Schema({
   name: {
       type: String,
       required: true,
+      minlength: 3,
       unique: true
   },
   number: {
       type: String,
       required: false,
+      minlength: 8,
       unique: false
   }
 })
